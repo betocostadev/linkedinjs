d377b706b810e908ae76a184164f1d46354f78fe
Beto Costa <betocostadev>
2020-02-06 10:24:21 -0300

    completed classes lesson

--- improve-js/classes/02_01/start/app.js
+++ improve-js/classes/02_01/start/app.js
@@ -1,37 +1,50 @@
+/* eslint-disable no-console */
 // this code will run fine as the function is hoisted
-sayHi();
+// CHECK THE USE OF THE SUPER() BELOW!!
+sayHi()
 
 class Car {
-    constructor(doors, engine, color) {
-        this.doors = doors;
-        this.engine = engine;
-        this.color = color;
-    }
+  constructor(doors, engine, color) {
+    this.doors = doors
+    this.engine = engine
+    this.color = color
+  }
 
-    carStats() {
-        return `This car has ${this.doors} doors, a ${this.engine} engine and a beautiful ${this.color} color!`;
-    }
+  carStats() {
+    return `This car has ${this.doors} doors, a ${this.engine} engine and a beautiful ${this.color} color!`
+  }
 
-    static totalDoors(car1, car2) {
-        const doors1 = car1.doors;
-        const doors2 = car2.doors;
+  static totalDoors(car1, car2) {
+    const doors1 = car1.doors
+    const doors2 = car2.doors
 
-        return doors1 + doors2;
-    }
+    return doors1 + doors2
+  }
 }
 
-const cx5 = new Car(4,'V6','grey');
-const civic = new Car(3,'V4','blue');
+// Using super
+class Suv extends Car {
+  constructor (doors, engine, color, baggageCap) {
+    super(doors, engine, color)
+    this.baggageCap = baggageCap
+  }
+}
 
-console.log(cx5);
-console.log(cx5.carStats());
-console.log(civic);
-console.log(civic.carStats());
-console.log(Car.totalDoors(cx5, civic));
+const m6 = new Suv(5, 'V8', 'black', 400)
+console.log(m6)
+
+const cx5 = new Car(4,'V6','grey')
+const civic = new Car(3,'V4','blue')
+
+console.log(cx5)
+console.log(cx5.carStats())
+console.log(civic)
+console.log(civic.carStats())
+console.log(Car.totalDoors(cx5, civic))
 
 
 function sayHi() {
-    return console.log('Hello this function can be called anywhere!');
+  return console.log('Hello this function can be called anywhere!')
 }
 
-sayHi();
+sayHi()
--- improve-js/classes/02_02/start/app.js
+++ improve-js/classes/02_02/start/app.js
@@ -1,27 +1,45 @@
+/* eslint-disable no-console */
 class Car {
-    constructor(doors, engine, color) {
-        this.doors = doors;
-        this.engine = engine;
-        this.color = color;
-    }
+  constructor(doors, engine, color) {
+    this.doors = doors
+    this.engine = engine
+    this.color = color
+  }
 
-    carStats() {
-        return `This car has ${this.doors} doors, a ${this.engine} engine and a beautiful ${this.color} color!`;
-    }
+  carStats() {
+    return `This car has ${this.doors} doors, a ${this.engine} engine and a beautiful ${this.color} color!`
+  }
 
-    static totalDoors(car1, car2) {
-        const doors1 = car1.doors;
-        const doors2 = car2.doors;
+  static totalDoors(car1, car2) {
+    const doors1 = car1.doors
+    const doors2 = car2.doors
 
-        return doors1 + doors2;
-    }
+    return doors1 + doors2
+  }
 }
 
-const cx5 = new Car(4,'V6','grey');
-const civic = new Car(3,'V4','blue');
+class Suv extends Car {
+  constructor (doors, engine, color, brand, carStats) {
+    super(doors, engine, color, carStats)
+    this.brand = brand
+    this.wheels = 4
+    this.ac = true
+  }
 
-console.log(cx5);
-// console.log(cx5.carStats());
+  myBrand() {
+    return console.log(`This Suv is a ${this.brand}`)
+  }
+}
+
+const cx5 = new Car(4,'V6','grey')
+const m6 = new Suv(4, 'V8', 'black', 'BMW')
+const civic = new Car(3,'V4','blue')
+
+console.log(cx5)
+console.log(m6)
+m6.myBrand()
+console.log(m6.carStats())
+console.log(cx5.carStats())
 // console.log(civic);
 // console.log(civic.carStats());
 // console.log(Car.totalDoors(cx5, civic));
