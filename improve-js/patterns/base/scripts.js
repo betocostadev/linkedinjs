1886fec4b82f399b8a443dd70464a1ab24c3b54a
Beto Costa <betocostadev>
2020-02-11 15:03:01 -0300

    started patterns

---
+++ improve-js/patterns/base/scripts.js
@@ -0,0 +1,89 @@
+/* eslint-disable no-console */
+
+/*
+// First class function:
+const calc = () => 4 * 3
+
+let aNumber = calc()
+
+console.log(aNumber)
+
+const printCalc = (callback) => {
+  console.log(callback())
+}
+
+printCalc(calc) */
+
+/* Prototype/Class and constructor design patterns */
+
+// class Character {
+//   constructor (serie, name, gender, age) {
+//     this.serie = serie
+//     this.name = name
+//     this.gender = gender
+//     this.age = age
+//   }
+// }
+
+// class Chartype extends Character {
+//   constructor(serie, name, gender, age, type) {
+//     super(serie, name, gender, age)
+//     this.type = type
+//   }
+// }
+
+// const lagertha = new Character('Vikings', 'Lagertha', 'female', 36)
+// const ragnar = new Chartype('Vikings', 'Ragnar', 'male', 40, 'Warrior')
+
+/* // Singleton Pattern
+let instance = null
+
+class Character {
+  constructor (serie, name, gender, age) {
+    if (!instance) {
+      this.serie = serie
+      this.name = name
+      this.gender = gender
+      this.age = age
+      instance = this
+    } else {
+      return instance
+    }
+  }
+}
+
+const loki = new Character('Vikings', 'Loki', 'male', 43)
+const lagertha = new Character('Vikings', 'Lagertha', 'female', 36)
+// The output will be for loki only.
+console.log(loki)
+console.log(lagertha) */
+
+// The Factor pattern
+
+class Car {
+  constructor (doors, engine, model) {
+    this.doors = doors
+    this.engine = engine
+    this.model = model
+  }
+}
+
+class carFactory {
+  createCar(type) {
+    switch(type) {
+    case 'sedan':
+      return new Car(4, 'V6', 'sedan')
+    case 'hatch':
+      return new Car(2, '4', 'hatch')
+    default:
+      return new Car(2, 'not selected', 'not selected')
+    }
+  }
+}
+
+const factory = new carFactory()
+const m3 = factory.createCar('sedan')
+const civic = factory.createCar('hatch')
+
+console.log(m3)
+console.log(civic)
