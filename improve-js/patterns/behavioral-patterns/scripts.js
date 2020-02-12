b0cffa0030a9f9c574cdc7fd9d507de530b3109f
Beto Costa <betocostadev>
2020-02-12 18:50:43 -0300

    more behavioral patterns

--- improve-js/patterns/behavioral-patterns/scripts.js
+++ improve-js/patterns/behavioral-patterns/scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* Behavioral patterns
  */
@@ -33,5 +34,80 @@ class Car {
 class consumption {
   update(car) {
     car.gas = car.gas + 1
+  }
+}
+
+/*
+// State pattern
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news1: {
+        type: 'top-headlines',
+        query: 'sources=bbc-news'
+      },
+      news2: {
+        type: 'everything',
+        query: 'domains=techcrunch.com&language=en'
+      },
+      news3: {
+        type: 'everything',
+        query: 'domains=comicbookmovie.com&language=en'
+      }
+    }
+  }
+}
+ */
+
+//  Iterator pattern
+// Loops
+
+/*
+// Strategy pattern
+class Car {
+  constructor(doors, engine, color) {
+      this.doors = doors;
+      this.engine = engine;
+      this.color = color;
+  }
+}
+
+class Suv extends Car {
+  constructor(doors, engine, color) {
+      super(doors, engine, color);
+      this.wheels = 4;
+  }
+}
+
+const civic = new Car(4, 'V6', 'grey');
+const cx5 = new Suv(4, "V8", 'red');
+*/
+
+// Mediator pattern
+
+class TrafficTower {
+  constructor() {
+    this.airplanes = []
+  }
+
+  requestPositions() {
+    return this.airplanes.map(airplane => {
+      return airplane.position
+    })
+  }
+}
+
+class Airplane {
+  constructor(position, trafficTower) {
+    this.position = position
+    this.trafficTower = trafficTower
+    this.trafficTower.airplanes.push(this)
+  }
+
+  requestPositions() {
+    return this.trafficTower.requestPositions()
   }
 }
